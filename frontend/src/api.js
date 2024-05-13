df581c31f533af3f4adea85df3a7dc34e7ff670d
a-naoufel <n_azizi@estin.com>
2024-05-13 23:20:11 +0100

    this

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const apiUrl = "/choreo-apis/hometech/backend/rest-api-be2/v1";
+const apiUrl = "/choreo-apis/";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
